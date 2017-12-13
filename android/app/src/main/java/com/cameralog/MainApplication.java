db220cfae8461817a2bd01153bb36e8231073be3
koishimasato <m.koishi@mugenup.com>
2017-12-13 23:27:28 +0900

    anddroid ライブラリリンク

--- android/app/src/main/java/com/cameralog/MainApplication.java
+++ android/app/src/main/java/com/cameralog/MainApplication.java
@@ -3,6 +3,8 @@ package com.cameralog;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import cl.json.RNSharePackage;
+import com.RNFetchBlob.RNFetchBlobPackage;
 import com.lwansbrough.RCTCamera.RCTCameraPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
@@ -24,6 +26,8 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new RNSharePackage(),
+            new RNFetchBlobPackage(),
             new RCTCameraPackage()
       );
     }
