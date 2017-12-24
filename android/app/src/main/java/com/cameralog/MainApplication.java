98d4d8a95f59f8da97f348530a15ba0811c8fda7
umeneri <mokushiroku97@gmail.com>
2017-12-24 12:49:45 +0900

    link

--- android/app/src/main/java/com/cameralog/MainApplication.java
+++ android/app/src/main/java/com/cameralog/MainApplication.java
@@ -3,6 +3,7 @@ package com.cameralog;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.oblador.vectoricons.VectorIconsPackage;
 import cl.json.RNSharePackage;
 import com.RNFetchBlob.RNFetchBlobPackage;
 import com.lwansbrough.RCTCamera.RCTCameraPackage;
@@ -26,6 +27,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new VectorIconsPackage(),
             new RNSharePackage(),
             new RNFetchBlobPackage(),
             new RCTCameraPackage()
